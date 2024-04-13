316056ff44ff7679b54e4f47a324266264c8104a
hgina0207 <hgina0207@gmail.com>
2024-04-13 21:40:39 +0900

    s

---
+++ 프로그래머스/입국심사.py
@@ -0,0 +1,17 @@
+import copy
+def solution(n, times):
+    answer=0
+    left=1
+    right=n*max(times)
+    while left<=right:
+        mid=(left+right)//2
+        people=0
+        for t in times:
+            people+=mid//t
+            if people>=n: break
+        if people>=n:
+            answer=mid
+            right=mid-1
+        else:
+            left=mid+1
+    return answer
