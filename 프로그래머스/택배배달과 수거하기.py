8fe8c923d2a402fd48546d6ddabec524234d0862
hgina0207 <hgina0207@gmail.com>
2024-05-02 21:11:00 +0900

    s

---
+++ 프로그래머스/택배배달과 수거하기.py
@@ -0,0 +1,13 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    deliver,pickup=0,0
+    
+    for i in range(n-1,-1,-1):
+        deliver+=deliveries[i]
+        pickup+=pickups[i]
+        
+        while deliver>0 or pickup>0:
+            deliver-=cap
+            pickup-=cap
+            answer+=(i+1)*2
+    return answer
